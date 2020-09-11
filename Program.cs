d971fc9dbd7e09a3d90fc07b1636461851e40dfb
stasyan <student@C251U39>
2020-09-11 17:49:08 +0300

    first chto-to

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace pr1_01_upr1
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
+      {
+        firstName = _firstName; lastName = _lastName; age = _age; gender = _gender;
+
+        {
+          firstName = _firstName;
+          lastName = _lastName; age = _age;
+        }
+
+
+      public override string ToString()
+      {
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
+      }
+
+
+
+    }
+
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+
+    }
+  }
